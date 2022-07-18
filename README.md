c024d2d41f0c476a65116ba7aa442837a3f14ee8
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-07-18 15:21:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#### Projeto trocando as cores
+
+##### Buscando sempre aprender desenvolvi esse projeto com javascript para troca de cores.
+
+#### Estilizei tamanhos com Css pratiquei o posicionamento dos elementos.
+
+[<img src="img/tela-cores.gif">]
+
+[<img src="img/cores.png">]
+
+### Tecnologias utilizadas
+<div flex-direction: row>
+
+ <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+
+  <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+  <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+
+  <br>
+  <br>
+
+[<img  height="150" width="150" src="img/marcia.jpeg"> ]
---
+++ index.js
@@ -0,0 +1,83 @@
+const buttons = document.getElementById('btn')
+
+const changeColor = (event) => {
+    changeColor[event.target.id]();
+
+
+}
+
+function verde(){
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('verde')
+
+}
+
+function vermelho() {
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('vermelho')
+
+}
+function azul() {
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('azul')
+
+}
+function laranja() {
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('laranja')
+
+}
+function roxo() {
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('roxo')
+
+
+}
+
+function rosa() {
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('marrom')
+    document.getElementById('exemplo').classList.add('rosa')
+
+}
+
+function marrom() {
+    document.getElementById('exemplo').classList.remove('verde')
+    document.getElementById('exemplo').classList.remove('vermelho')
+    document.getElementById('exemplo').classList.remove('laranja')
+    document.getElementById('exemplo').classList.remove('roxo')
+    document.getElementById('exemplo').classList.remove('azul')
+    document.getElementById('exemplo').classList.remove('rosa')
+    document.getElementById('exemplo').classList.add('marrom')
+}
+
+buttons.addEventListener('click', changeColor )
